6c1073cd87f490d31859a0344bd4b88910af648d
Atanasov_Andreea <andrreaat@gmail.com>
2024-04-26 12:26:02 +0300

    am facut butonul click for andreea

--- js/script.js
+++ js/script.js
@@ -2,8 +2,16 @@ const find = (selector) => {
     return document.querySelector(selector);
 }
 
-const vladBtn = find("#vlad");
 
-vladBtn.addEventListener("click", ()=>{
-    alert("Hello vlad!");
+// const vladBtn = find("#vlad");
+const andreeaBtn = find('.andreea')
+
+//
+// vladBtn.addEventListener("click", ()=>{
+//     alert("Hello vlad!");
+// })
+
+andreeaBtn.addEventListener('click',()=>{
+    alert('Hello,Andreea!')
+    console.log(1)
 })
